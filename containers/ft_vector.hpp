1cf222e7d1d2f15b6d6beb0f114a3e387ff444fb
jhonatan229 <jhonatan229@outlook.com>
2022-08-01 19:06:36 +0200

    creating some features for vector container

--- containers/ft_vector.hpp
+++ containers/ft_vector.hpp
@@ -79,6 +79,121 @@ namespace ft
 			}
 			return *this;
 		}
+
+		reference operator[] (size_type n){
+			return(*(this->_vec + n));
+		}
+
+		const reference operator[](size_type n) const{
+			return(*(this->_vec + n));
+		}
+
+		iterator begin() {
+				return iterator(this->_vec);
+			}
+
+			const_iterator begin() const {
+				return const_iterator(this->_vec);
+			}
+
+			iterator end() {
+				return begin() + this->_size;
+			}
+
+			const_iterator end() const {
+				return begin() + this->_size;
+			}
+
+			reverse_iterator rbegin() {
+				return reverse_iterator(end());
+			}
+
+			const_reverse_iterator rbegin() const {
+				return const_reverse_iterator(end());
+			}
+
+			reverse_iterator rend() {
+				return reverse_iterator(begin());
+			}
+
+			const_reverse_iterator rend() const {
+				return const_reverse_iterator(begin());
+			}
+
+			size_type size() const {
+				return this->_size;
+			}
+
+			size_type max_size() const {
+				return this->_alloc.max_size();
+			}
+
+			size_type capacity()const{
+				return this->_capacity;
+			}
+
+			bool empty() const{
+				return this->_size == 0;
+			}
+
+			reference at(size_type n){
+				if(n >= this->_size)
+					throw std::out_of_range("ft::vector::at is out of range");
+				return (*(this->_vec + n));
+			}
+
+			const_reference at(size_type n) const{
+				if(n >= this->_size)
+					throw std::out_of_range("ft::vector::at is out of range");
+				return (*(this->_vec + n));
+			}
+
+			reference front(){
+				return this->_vec[0];
+			}
+
+			const_reference front() const{
+				return this->_vec[0];
+			}
+
+			reference back(){
+				return this->_vec[this->_size - 1];
+			}
+
+			const_reference back() const{
+				return this->_vec[this->_size - 1];
+			}
+
+			/** Resize faz oq o nome diz, reajusta o tamanho da lista, dependendo do tamanho 'n';
+			 * se for maior que o tamanho atual da lista, é alocado mais memoria para a lista e adicionado
+			 * o 'val' em cada espaço adicional;
+			 * Caso o n for menor, noss lista vai diminuir, destruindo essas alocações extras que não são mais
+			 * nesecitadas;
+			*/
+			void resize(size_type n, value_type val = value_type()){
+				if(n > this->_size){
+					reserve(n);
+					for (size_type i = this->_size; i < n; i++)
+						this->_alloc.construct(this->_vec + i, val);
+				}
+				else if(n < this->_size)
+					for (size_type i = n; i < this->_size; i++)
+						this->_alloc.destroy(this->_vec + i);
+			}
+
+			void reseve(size_type n){
+				if (n > max_size())
+					throw std::length_error("ft::vector::reserve:: n is greates than max size");
+				if (n > _capacity) {
+					this->_capacity = n;
+					pointer tmp = this->_alloc.allocate(this->_capacity);
+					for (size_type i = 0; i < this->_size; i++)
+						this->_alloc.construct(tmp + i, *(this->_vec + i));
+					this->_alloc.deallocate(this->_vec, this->_size);
+					this->_vec = tmp;
+				}
+			}
+
 	};
 }
 
