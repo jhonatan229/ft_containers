345855b56c17327e67ed97cb8d490f459a47a447
jhonatan229 <jhonatan229@outlook.com>
2022-08-18 18:47:03 -0300

    up

--- includes/ft_rb_tree.hpp
+++ includes/ft_rb_tree.hpp
@@ -1,15 +1,39 @@
 #ifndef FT_RB_TREE_HPP
-# define FT_RB_TREE_HPP
+#define FT_RB_TREE_HPP
 
-#include <reverse_iterator.hpp>
+#include <ft_reverse_iterator.hpp>
 #include <utility.hpp>
-#include <algorithm.hpp>
 #include <functional.hpp>
+#include <algorithm.hpp>
 #include <memory>
-#include <rb_tree_iterator.hpp>
+#include <ft_rb_tree_iterator.hpp>
 
-namespace ft{
-	te
-}
+namespace ft
+{
+	template <class Key, class T, class KeyOfValue, class Compare = ft::less<Key>, class Alloc = std::allocator<ft::pair<const Key, T>>>
+	class rb_tree
+	{
 
-#endif 
+	public:
+		typedef Key key_type;
+		typedef T mapped_type;
+		typedef T *pointer;
+		typedef T &reference;
+		typedef const T *const_pointer;
+		typedef const T &const_reference;
+		typedef Alloc allocator_type;
+		typedef Compare key_compare;
+		typedef typename Alloc::value_type value_type;
+		typedef Node<value_type, allocator_type> node_type;
+		typedef node_type *ptr_node;
+		typedef node_type const &ref_node;
+		typedef std::size_t size_type;
+		typedef std::ptrdiff_t difference_type;
+		typedef rbt_iterator<value_type> iterator;
+		typedef rbt_const_iterator<value_type> const_iterator;
+		typedef ft::reverse_iterator<iterator> reverse_iterator;
+		typedef ft::reverse_iterator<const_iterator> const_reverse_iterator;
+		typedef std::allocator<node_type> node_allocator_type;
+	}
+
+#endif
