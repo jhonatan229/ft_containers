c0a58050af6ae8c154667e582d2a1ef7d1490d5b
jhonatan229 <jhonatan229@outlook.com>
2022-09-21 15:53:44 -0300

    update main

--- tests/my_test.cpp
+++ tests/my_test.cpp
@@ -1,4 +1,5 @@
 #include <tests.hpp>
+#include <sys/time.h>
 
 int main(int argc, char **argv)
 {
@@ -7,23 +8,32 @@ int main(int argc, char **argv)
 		return 1;
 	if (argv[1][0] == '1')
 	{
-		// time_t now = time(nullptr);
-		// time_t mnow = now * 1000;
+		struct timeval tp;
+		gettimeofday(&tp, NULL);
+		long int mbefore = tp.tv_sec * 1000 + tp.tv_usec / 1000;
 		make_my_check_type_test();
 		make_my_iterator_test();
 		make_my_map_test();
 		make_my_vector_test();
 		make_my_stack_test();
 		make_my_perform_test();
-		// time_t before = time(nullptr);
-		// time_t mbefore = before * 1000;
-		// std::cout << "\033[1;32mTOTAL TIME: " << (mbefore - mnow) <<"\033[0m" << std::endl;
+		struct timeval after;
+		gettimeofday(&after, NULL);
+		long int mafter = after.tv_sec * 1000 + after.tv_usec / 1000;
+		std::cout << "\033[1;32mTOTAL TIME: " << (mafter - mbefore) <<" miliseconds\033[0m" << std::endl;
 	}
 	else if (argv[1][0] == '2')
 	{
+		struct timeval tp;
+		gettimeofday(&tp, NULL);
+		long int mbefore = tp.tv_sec * 1000 + tp.tv_usec / 1000;
 		make_stl_iterator_test();
 		make_stl_map_test();
 		make_stl_vector_test();
 		make_stl_perform_test();
+		struct timeval after;
+		gettimeofday(&after, NULL);
+		long int mafter = after.tv_sec * 1000 + after.tv_usec / 1000;
+		std::cout << "\033[1;32mTOTAL TIME: " << (mafter - mbefore) <<" miliseconds\033[0m" << std::endl;
 	}
 }
